ca7b486dab6fe5c903f66e1d8375ee6e062c9c88
Louis Smit <louissm.it@icloud.com>
2023-09-07 06:22:11 +0200

    Remove token from response

--- src/todos.ts
+++ src/todos.ts
@@ -3,14 +3,25 @@ import { InsertTodo, todos } from '~/schema'
 import { db } from '~/utils/db'
 
 export async function getTodos(token: string) {
-  return await db.select().from(todos).where(eq(todos.token, token))
+  return await db
+    .select({
+      id: todos.id,
+      title: todos.title,
+      completed: todos.completed,
+    })
+    .from(todos)
+    .where(eq(todos.token, token))
 }
 
 export async function createTodo(todo: InsertTodo, token: string) {
   return db
     .insert(todos)
     .values({ ...todo, token })
-    .returning()
+    .returning({
+      id: todos.id,
+      title: todos.title,
+      completed: todos.completed,
+    })
 }
 
 export async function updateTodo(id: number, todo: InsertTodo, token: string) {
@@ -18,5 +29,9 @@ export async function updateTodo(id: number, todo: InsertTodo, token: string) {
     .update(todos)
     .set({ ...todo, token })
     .where(and(eq(todos.id, id), eq(todos.token, token)))
-    .returning()
+    .returning({
+      id: todos.id,
+      title: todos.title,
+      completed: todos.completed,
+    })
 }
